518d0c7b44fef8975946149ff38f6c176ca31a8a
Oscar Raig <oscar.raig@gmail.com>
2016-10-25 00:16:01 +0200

    Remove useless tests

--- app/src/androidTest/java/com/example/oscarraig/justjava/MainActivityTest.java
+++ app/src/androidTest/java/com/example/oscarraig/justjava/MainActivityTest.java
@@ -25,34 +25,5 @@ public class MainActivityTest extends
       protected void setUp() throws Exception {
           super.setUp();
       }
-
-    @SmallTest
-    public void testFirst(){
-        assertTrue(false);
-    }
-
-    public void incrementTextWhendisplayIsCalled() {
-        MainActivity mainActivity = getActivity();
-        mainActivity.submitOrder(null);
-        TextView quantityTextView = (TextView) mainActivity
-                .findViewById(R.id.quantity_text_view);
-        Integer quantity = Integer.parseInt(quantityTextView.getText().toString());
-        assertThat(quantity,is(1));
-
-    }
-
-    @UiThreadTest
-    public void testSetTextWithAnnotation() throws Exception {
-
-        MainActivity activity = getActivity();
-
-        // search for the textView
-        final TextView quantityTextView = (TextView) activity
-                .findViewById(R.id.quantity_text_view);
-
-        Integer quantity = Integer.parseInt(quantityTextView.getText().toString());
-        assertThat(quantity,is(1));
-
-    }
-
+    
 }
