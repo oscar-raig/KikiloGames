85476722798f64cfd95a328ee0163eb3bdffd2c4
Oscar Raig <oscar.raig@gmail.com>
2016-11-06 18:47:23 +0100

    fixing travis error

--- app/src/main/java/com/example/oscarraig/justjava/controller/ForecastFragment.java
+++ app/src/main/java/com/example/oscarraig/justjava/controller/ForecastFragment.java
@@ -69,15 +69,7 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Create some dummy data for the ListView.  Here's a sample weekly forecast
-        String[] data = {
-                "Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
-        };
+        String[] data = {"waiting for data ..."};
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
 
 
@@ -97,7 +89,7 @@ public class ForecastFragment extends Fragment {
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
 
-
+        getForeCast();
         return rootView;
 
     }
