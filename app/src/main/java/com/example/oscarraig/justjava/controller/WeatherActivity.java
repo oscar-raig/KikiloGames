70790e0a0dc54c9e76cb506754008db3ac13c661
Oscar Raig <oscar.raig@gmail.com>
2016-11-06 18:17:14 +0100

    Weather

--- app/src/main/java/com/example/oscarraig/justjava/controller/WeatherActivity.java
+++ app/src/main/java/com/example/oscarraig/justjava/controller/WeatherActivity.java
@@ -1,17 +1,25 @@
 package com.example.oscarraig.justjava.controller;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+
 
 import com.example.oscarraig.justjava.R;
 
 
-
 public class WeatherActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        if(savedInstanceState == null ) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .add(R.id.container, new ForecastFragment()).commit();
+        }
+
     }
 
 }
